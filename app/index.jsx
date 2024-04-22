af1863333b9d24ae381d59c4b838ba62ef93e57d
KarlStevenRequinto <lerdrobott@gmail.com>
2024-04-22 18:52:39 +0800

    tabs layout

--- app/index.jsx
+++ app/index.jsx
@@ -7,8 +7,8 @@ const RootLayout = () => {
         <View className="flex-1 items-center justify-center bg-white">
             <Text className="text-3xl font-pblack">VidShare</Text>
             <StatusBar style="auto" />
-            <Link href="/profile" style={{ color: "blue" }}>
-                Go To Profile
+            <Link href="/home" style={{ color: "blue" }}>
+                Go To Home
             </Link>
         </View>
     );
