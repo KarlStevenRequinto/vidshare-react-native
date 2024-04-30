16e0ff35099be55d12d4f53e4b9e34857d9ee1d4
KarlStevenRequinto <lerdrobott@gmail.com>
2024-05-01 03:26:52 +0800

    adding components for homescreen, useappwrite hook, homescreen initial progress

--- components/index.js
+++ components/index.js
@@ -3,8 +3,8 @@ import FormField from "./FormField";
 import CustomButton from "./CustomButton";
 // import InfoBox from "./InfoBox";
 // import Loader from "./Loader";
-// import Trending from "./Trending";
-// import SearchInput from "./SearchInput";
-// import EmptyState from "./EmptyState";
+import Trending from "./Trending";
+import SearchInput from "./SearchInput";
+import EmptyState from "./EmptyState";
 
-export { FormField, CustomButton };
+export { FormField, CustomButton, Trending, SearchInput, EmptyState };
